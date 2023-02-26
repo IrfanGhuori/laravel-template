8996a9f2c95c7962203fc2e3dc01a6e13d31b68b
Irfan <webdeveloper1119@gmail.com>
2023-02-26 08:36:29 +0500

    updated

--- app/Http/Controllers/ContactUs.php
+++ app/Http/Controllers/ContactUs.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+
 
 class ContactUs extends Controller
 {
@@ -12,19 +12,19 @@ class ContactUs extends Controller
     public function CollectData(Request $req) 
     {
         $validated = $req->validate([   
-            'uEmail'             => 'required|unique:members',
-            'uUser'          => 'required|min:4',
-            'uDetails'      => 'required|min:10'
+            'uEmail' => 'required|email:rfc',
+            'uUser' => 'required|min:4',
+            'uDetails' => 'required|min:10'
         ],
         [   
             'uEmail.required'    => 'Please Provide Your Email Address For Better Communication, Thank You.',
-            'uEmail.unique'      => 'Sorry, This Email Address Is Already Used By Another User. Please Try With Different One, Thank You.',
+            'uEmail.email'      => 'The your email field must be a valid email address.',
             'uUser.required' => 'Username Is Required For Your Information Safety, Thank You.',
             'uUser.min'      => 'Username Length Should Be More Than 8 Character Or Digit Or Mix, Thank You.',
             'uDetails.required' => 'Details Is Required For Your Information Safety, Thank You.',
             'uDetails.min'      => 'Details Length Should Be More Than 10 Character Or Digit Or Mix, Thank You.'
         ]);
-        // return redirect('/contact');    
+            
     }
 
      
